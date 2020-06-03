d299ae06e005fe7d10d729d0d287a1391dc53fe4
Joao <jvictorsouto18@gmail.com>
2020-06-03 20:49:48 -0300

    projeto completo

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,21 +1,79 @@
-class DateHelper {
+'use strict';
 
-  constructor() {
+System.register([], function (_export, _context) {
+    "use strict";
 
-    throw new Error('Esta classe não pode ser instanciada');
-  }
+    var _createClass, DateHelper;
 
-  static dataParaTexto(data) {
+    function _toConsumableArray(arr) {
+        if (Array.isArray(arr)) {
+            for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+                arr2[i] = arr[i];
+            }
 
-    return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
-  }
+            return arr2;
+        } else {
+            return Array.from(arr);
+        }
+    }
 
-  static textoParaData(texto) {
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
 
-    if (!/\d{2}\/\d{2}\/\d{4}/.test(texto))
-      throw new Error('Deve estar no formato dd/mm/aaaa');
+    return {
+        setters: [],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
 
-    // veja que usamos no split '/' no lugar de '-'. Usamos `reverse` também para ficar ano/mes/dia.
-    return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
-  }
-}
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
+
+            _export('DateHelper', DateHelper = function () {
+                function DateHelper() {
+                    _classCallCheck(this, DateHelper);
+
+                    throw new Error('Esta classe não pode ser instanciada');
+                }
+
+                _createClass(DateHelper, null, [{
+                    key: 'dataParaTexto',
+                    value: function dataParaTexto(data) {
+
+                        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+                    }
+                }, {
+                    key: 'textoParaData',
+                    value: function textoParaData(texto) {
+
+                        if (!/\d{2}\/\d{2}\/\d{4}/.test(texto)) throw new Error('Deve estar no formato dd/mm/aaaa');
+
+                        return new (Function.prototype.bind.apply(Date, [null].concat(_toConsumableArray(texto.split('/').reverse().map(function (item, indice) {
+                            return item - indice % 2;
+                        })))))();
+                    }
+                }]);
+
+                return DateHelper;
+            }());
+
+            _export('DateHelper', DateHelper);
+        }
+    };
+});
+//# sourceMappingURL=DateHelper.js.map
--- client/js/app/models/Mensagem.js
+++ client/js/app/models/Mensagem.js
@@ -1,16 +1,63 @@
-class Mensagem {
+'use strict';
 
-  constructor(texto) {
-    this._texto = texto || ''; // se texto for undefined, vai passar ''
-  }
+System.register([], function (_export, _context) {
+    "use strict";
 
-  get texto() {
+    var _createClass, Mensagem;
 
-    return this._texto;
-  }
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
 
-  set texto(texto) {
+    return {
+        setters: [],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
 
-    this._texto = texto;
-  }
-}
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
+
+            _export('Mensagem', Mensagem = function () {
+
+                // constructor(texto='') {
+                function Mensagem(texto) {
+                    _classCallCheck(this, Mensagem);
+
+                    this._texto = texto || '';
+                }
+
+                _createClass(Mensagem, [{
+                    key: 'texto',
+                    get: function get() {
+
+                        return this._texto;
+                    },
+                    set: function set(texto) {
+
+                        this._texto = texto;
+                    }
+                }]);
+
+                return Mensagem;
+            }());
+
+            _export('Mensagem', Mensagem);
+        }
+    };
+});
+//# sourceMappingURL=Mensagem.js.map
